de08b63d60fdcdcfe4e530f1c87597dad24d5b4b
yysach <yysachin11@gmail.com>
2020-06-02 17:48:35 +0530

    here using dynamodb table for hit count

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,24 +1,39 @@
 from aws_cdk import (
-    aws_iam as iam,
+    core,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
-    core
 )
+
+from hitcounter import HitCounter
+from cdk_dynamo_table_viewer import TableViewer
+
 
 class CdkworkshopStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        my_lambda= _lambda.Function(
-        self,'HelloHandler',
-        runtime=_lambda.Runtime.PYTHON_3_6,
-        code=_lambda.Code.asset('lambda'),
-        handler='hello.handler',
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
+        )
+
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
         )
 
         apigw.LambdaRestApi(
-        self,'Endpoint',
-        handler=my_lambda,
+            self, 'Endpoint',
+            handler=hello_with_counter.handler,
         )
-        # nothing here
+
+
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
+        )
